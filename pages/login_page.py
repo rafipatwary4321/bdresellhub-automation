fa00238cd442ecffa3ba427503367d665451f292
Your Name <patoary23@gmail.com>
2026-04-30 01:01:45 +0600

    Stabilize login selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,14 +3,14 @@ from playwright.sync_api import Page
 class LoginPage:
     def __init__(self, page: Page):
         self.page = page
-        self.email_input = 'input[type="email"]'
-        self.password_input = 'input[type="password"]'
-        self.login_button = 'button[type="submit"]'
+        self.email_input = 'input[name="email"], input[type="email"]'
+        self.password_input = 'input[name="password"], input[type="password"]'
+        self.login_button = 'button[type="submit"], button:has-text("Login"), button:has-text("Sign In")'
 
     def load(self):
-        self.page.goto("https://bdresellhub.com/login")
+        self.page.goto("https://bdresellhub.com/login", wait_until="networkidle")
 
     def login(self, email, password):
-        self.page.fill(self.email_input, email)
-        self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
+        self.page.locator(self.email_input).first.fill(email)
+        self.page.locator(self.password_input).first.fill(password)
+        self.page.locator(self.login_button).first.click()
