7a042cfe31470af894bfa25b00912e5c0c64c7f7
Your Name <patoary23@gmail.com>
2026-04-30 00:18:48 +0600

    Refactor FAQ test to use pytest fixture

--- tests/test_faq.py
+++ tests/test_faq.py
@@ -1,20 +1,9 @@
-from playwright.sync_api import sync_playwright
+def test_faq_page(page):
+    page.goto("https://bdresellhub.com/faq")
 
-def test_faq_page():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    assert page.locator("body").is_visible()
 
-        # FAQ page open
-        page.goto("https://bdresellhub.com/faq")
+    faq_items = page.locator("button")
 
-        # Page load check
-        assert page.locator("body").is_visible()
-
-        # First FAQ item click
-        faq_items = page.locator("button")
-
-        if faq_items.count() > 0:
-            faq_items.nth(0).click()
-
-        browser.close()
+    if faq_items.count() > 0:
+        faq_items.nth(0).click()
